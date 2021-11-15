4b43b03eb0ab5afd94a4df0b9718dc59c4357664
LoneHippie <lonehippie.music@gmail.com>
2021-11-15 20:06:18 +0200

    added and finished UI for editions page

---
+++ src/pages/editions.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from './../components/Layout';
+import Seo from './../components/Seo';
+import EditionCardLarge from './../components/EditionCardLarge';
+
+import * as classes from './editions.module.scss';
+
+const EditionsPage = ({ data }) => {
+
+    const sortedEditions = data.editions.edges.reverse();
+
+    return (
+        <Layout darkNavIcons={true} darkFooterBackground={true}>
+            <Seo title="Editions" />
+
+            <header className={classes.header}>
+                <h1>Platform Editions</h1>
+            </header>
+
+            <section className={classes.editions_section}>
+                {
+                    sortedEditions.map((el, index) => (
+                        <EditionCardLarge 
+                            key={`edition-card-${index}`}
+                            edition={el.node}
+                            isFeatured={false}
+                            isLatest={index === 0 ? true : false}
+                        />
+                    ))
+                }
+            </section>
+
+            <a 
+                className={classes.button}
+                href="/catalogue" 
+                alt="Go to article and interview archive"
+            >
+                {`Browse Articles \u2192`}
+            </a>
+
+        </Layout>
+    )
+};
+
+export default EditionsPage;
+
+export const pageQuery = graphql`
+    query {
+        editions: allContentfulEdition(sort: {fields: editionNumber}) {
+            edges {
+                node {
+                    editionTitle
+                    coverImage {
+                        file {
+                            url
+                        }
+                    }
+                    editionMonth(formatString: "MMMM YYYY")
+                    editionSummary {
+                        editionSummary
+                    }
+                    slug
+                }
+            }
+        }
+    }
+`;
