4984ef9f37e81869760f4f5d7b7cbc678c428430
LoneHippie <lonehippie.music@gmail.com>
2021-11-24 15:02:22 +0200

    fixed text formatting for preview cards that needed md to html conversion

--- src/components/InterviewCardLarge.js
+++ src/components/InterviewCardLarge.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import showdown from 'showdown';
 
 import * as classes from './InterviewCardLarge.module.scss';
 
 const InterviewCardLarge = ({ interview }) => {
+
+    const formattedText = () => {
+        let converter = new showdown.Converter();
+        let HTML = converter.makeHtml(interview.text.text);
+        return HTML.slice(0, 160).trim().replace(/<p>/g, '') + '...';
+    };
 
     return (
         <article className={classes.card}>
@@ -13,7 +20,10 @@ const InterviewCardLarge = ({ interview }) => {
             />
             <h4 className={classes.title}>{interview.title}</h4>
             <h5 className={classes.subtitle}>{interview.subtitle}</h5>
-            <p className={classes.preview_text}>{`${interview.text.text.slice(0, 160).trim()}...`}</p>
+            <p 
+                className={classes.preview_text}
+                dangerouslySetInnerHTML={{ __html: formattedText() }}
+            />
             <a
                 href={interview.isPartOfEdition ? `/${interview.editionSlug}/${interview.slug}` : `/interview/${interview.slug}`}
                 alt={'Read the full interview here'}
